e51978d6424fb36dc73443f355d556d8f82f4bd5
Joao Rolo <roloje777@gmail.com>
2022-12-20 11:02:34 +0000

    add profiles Lesson 38

--- src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -1,9 +1,6 @@
 package guru.springframework.sfgdi;
 
-import guru.springframework.sfgdi.controllers.ConstructorInjectedController;
-import guru.springframework.sfgdi.controllers.MyController;
-import guru.springframework.sfgdi.controllers.SetterInjectedController;
-import guru.springframework.sfgdi.controllers.PropertyInjectionController;
+import guru.springframework.sfgdi.controllers.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -16,9 +13,13 @@ public class SfgDiApplication {
 		//SpringApplication.run(SfgDiApplication.class, args);
 		// return a context from the application
 		ApplicationContext ctx = SpringApplication.run(SfgDiApplication.class, args);
-	    /*
+
+		I18nController i18nController= (I18nController) ctx.getBean("i18nController");
+		System.out.println(i18nController.sayHello());
+
+		/*
 	       return a reference to the controller from the context
-	       by default the name is the same, bu starts with lowercase
+	       by default the name is the same, but starts with lowercase
 	       Must cast as it's an Object type returned
 	     */
 		MyController myController = (MyController)ctx.getBean("myController");
