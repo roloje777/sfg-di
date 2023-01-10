8478f44a265375bbe365cbb0fe4697535848f69d
Joao Rolo <roloje777@gmail.com>
2023-01-10 09:55:38 +0000

    clean up un-used imports

--- src/main/java/guru/springframework/sfgdi/services/I18nEnglishGreetingService.java
+++ src/main/java/guru/springframework/sfgdi/services/I18nEnglishGreetingService.java
@@ -1,7 +1,6 @@
 package guru.springframework.sfgdi.services;
 
-import org.springframework.context.annotation.Profile;
-import org.springframework.stereotype.Service;
+
 
 /**
  * Created by jt on 12/27/19.
