c40da434e22afc8f6d0d9fb6a6f1879fef88b6ce
Joao Rolo <roloje777@gmail.com>
2023-01-12 09:48:13 +0000

    clean code.

--- src/main/java/com/springframework/pets/CatPetService.java
+++ src/main/java/com/springframework/pets/CatPetService.java
@@ -1,14 +1,11 @@
 package com.springframework.pets;
 
-//import org.springframework.context.annotation.Profile;
-//import org.springframework.stereotype.Service;
 
 /**
  * Created by jt on 12/28/19.
  */
 
-//@Profile("cat")
-//@Service()
+
 public class CatPetService implements PetService {
 
 
