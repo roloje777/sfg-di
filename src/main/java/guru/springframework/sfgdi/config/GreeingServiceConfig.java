db342d02fd69ee0bf10f1e5e751c9f2326df7514
Joao Rolo <roloje777@gmail.com>
2023-01-12 05:58:46 +0000

    add dependency injection in Java Configuration

--- src/main/java/guru/springframework/sfgdi/config/GreeingServiceConfig.java
+++ src/main/java/guru/springframework/sfgdi/config/GreeingServiceConfig.java
@@ -1,8 +1,9 @@
 package guru.springframework.sfgdi.config;
 
+import guru.springframework.sfgdi.repositories.EnglishGreetingRepository;
+import guru.springframework.sfgdi.repositories.EnglishGreetingRepositoryImpl;
 import guru.springframework.sfgdi.services.*;
 
-import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -31,10 +32,14 @@ public class GreeingServiceConfig {
         return new PrimaryGreetingService();
     }
 
+    @Bean
+    EnglishGreetingRepository englishGreetingRepository(){
+        return new EnglishGreetingRepositoryImpl();
+    }
     @Profile("EN")
     @Bean()
-    I18nEnglishGreetingService i18nService(){
-        return new I18nEnglishGreetingService();
+    I18nEnglishGreetingService i18nService(EnglishGreetingRepository englishGreetingRepository){
+        return new I18nEnglishGreetingService(englishGreetingRepository);
     }
 
     @Profile({"ES", "default"})
--- src/main/java/guru/springframework/sfgdi/services/I18nEnglishGreetingService.java
+++ src/main/java/guru/springframework/sfgdi/services/I18nEnglishGreetingService.java
@@ -1,6 +1,7 @@
 package guru.springframework.sfgdi.services;
 
 
+import guru.springframework.sfgdi.repositories.EnglishGreetingRepository;
 
 /**
  * Created by jt on 12/27/19.
@@ -8,8 +9,18 @@ package guru.springframework.sfgdi.services;
 
 
 public class I18nEnglishGreetingService implements GreetingService {
+
+    private final EnglishGreetingRepository englishGreetingRepository;
+
+    public I18nEnglishGreetingService(EnglishGreetingRepository englishGreetingRepository) {
+        this.englishGreetingRepository = englishGreetingRepository;
+    }
+
     @Override
     public String sayGreeting() {
-        return "Hello World - EN";
+
+        // I changed this as John did not use the actual repo here
+        //return "Hello World - EN";
+        return englishGreetingRepository.getGreeting();
     }
 }
