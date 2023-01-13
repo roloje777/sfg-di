fd3ab12435d93f65e27b4f911d1ed3667926b412
Joao Rolo <roloje777@gmail.com>
2023-01-13 07:03:24 +0000

    add Bean Scope example.

---
+++ src/main/java/guru/springframework/sfgdi/services/SingletonBean.java
@@ -0,0 +1,16 @@
+package guru.springframework.sfgdi.services;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class SingletonBean {
+
+    public SingletonBean() {
+        System.out.println("Creating a Singleton Bean!");
+    }
+
+    public String getMyScope(){
+        return "I'm Singleton";
+
+    }
+}
